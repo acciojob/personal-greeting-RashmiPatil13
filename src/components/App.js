30cecb4a7532041ed3a1ddd106e2d958b0dd4e57
RashmiPatil13 <rashmipatil1113@gmail.com>
2023-12-13 16:39:57 +0530

    code

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,26 @@
 
-import React from "react";
+import React,{useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [text,setText] = useState("") // ab
+
+    console.log("text",text) // a
+
+    function getValue(event){
+        
+         setText(event.target.value) //ab
+    }
   return (
     <div>
         {/* Do not remove the main div */}
+        
+            <form>
+                <h1>Enter your name :</h1>
+                <input type="text" onChange={getValue}/>
+            </form>
+            <p> Hello{text}</p>
+        
     </div>
   )
 }
